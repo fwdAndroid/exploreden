a725485388250f224d6550b6ce5200436b8d6426
fawad kaleem <fwdkaleem@gmail.com>
2023-10-19 13:53:39 +0500

    Lcoation

--- lib/screens/dashboard/pages/message_page.dart
+++ lib/screens/dashboard/pages/message_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:exploreden/screens/detail/location_detail.dart';
 import 'package:exploreden/utils/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -12,41 +14,82 @@ class _MessagePageState extends State<MessagePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          automaticallyImplyLeading: false,
-          centerTitle: true,
-          title: const Text(
-            "Message",
-          )),
-      body: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-                onTap: () {
-                  // Navigator.push(
-                  //     context,
-                  //     MaterialPageRoute(
-                  //         builder: (builder) => DestinationPage()));
-                },
-                leading: CircleAvatar(
-                  backgroundImage: AssetImage("assets/face.png"),
-                ),
-                trailing: Text(
-                  "11:30",
-                  style:
-                      TextStyle(color: colorBlack, fontWeight: FontWeight.bold),
-                ),
-                subtitle: Text(
-                  "I’m not a hoarder but I really...",
-                  style:
-                      TextStyle(color: colorBlack, fontWeight: FontWeight.bold),
-                ),
-                title: Text(
-                  "Siliva",
-                  style:
-                      TextStyle(color: colorBlack, fontWeight: FontWeight.bold),
-                ));
-          }),
-    );
+        appBar: AppBar(
+            automaticallyImplyLeading: false,
+            centerTitle: true,
+            title: const Text(
+              "Explorer Den Locations",
+            )),
+        body: SizedBox(
+          height: 225,
+          child: StreamBuilder(
+              stream:
+                  FirebaseFirestore.instance.collection("location").snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.data!.docs.isEmpty) {
+                  return const Center(
+                    child: Text(
+                      "No Locations Added by the Explorer Den Found yet",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  );
+                }
+                return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return StreamBuilder<Object>(
+                          stream: FirebaseFirestore.instance
+                              .collection("location")
+                              .snapshots(),
+                          builder:
+                              (BuildContext context, AsyncSnapshot snapshot) {
+                            if (!snapshot.hasData) {
+                              return const Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+                            final List<DocumentSnapshot> documents =
+                                snapshot.data!.docs;
+                            final Map<String, dynamic> data =
+                                documents[index].data() as Map<String, dynamic>;
+                            return Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Card(
+                                  child: ListTile(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (builder) =>
+                                                  LocationDetail(
+                                                    name: data['name'],
+                                                    description:
+                                                        data['location'],
+                                                    address: data['address'],
+                                                  )));
+                                    },
+                                    title: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text("Location Name"),
+                                        Text(data['name'])
+                                      ],
+                                    ),
+                                    trailing: Icon(
+                                      Icons.arrow_forward_ios,
+                                      color: colorBlack,
+                                    ),
+                                  ),
+                                ));
+                          });
+                    });
+              }),
+        ));
   }
 }
