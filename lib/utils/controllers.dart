fc901dea334602752aad5d6a0ecaf75c2168311a
fawad kaleem <fwdkaleem@gmail.com>
2023-10-19 08:56:26 +0500

    Controllers Done

---
+++ lib/utils/controllers.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+final signUpemailControllers = TextEditingController();
+final signUppassControllers = TextEditingController();
+
+//Login
+final loginEmailControllers = TextEditingController();
+final loginPasswordControllers = TextEditingController();
